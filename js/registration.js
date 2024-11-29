708de3d8075d82be1b0c2737cbbd3292fa65f692
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2024-11-29 15:46:20 +0530

    all js added

--- js/registration.js
+++ js/registration.js
@@ -1,5 +1,4 @@
 const rowname=[]
-const rownumber=[]
 
  function addname(){
     const addcontainer = document.getElementById("nameflexbox")
@@ -10,26 +9,12 @@ const rownumber=[]
 }
 addname();
 
-function addnumber(){
-  const addcontainer = document.getElementById("nameflexbox")
-  addcontainer.innerHTML=""
-  for(i=0;i<rownumber.length;i++){
-  addcontainer.innerHTML += `<div class="add-number"> ${i+1+")"} ${rownumber[i]} </div>`
-  }
-}
-addnumber();
-
-
   function addrow(){
     const name = document.getElementById("name");
     rowname.push(name.value)
     name.value="";
     addname();
   }
+
+  
  
-  function addrownumber(){
-    const number = document.getElementById("number");
-    rownumber.push(number.value)
-    number.value="";
-    addnumber();
-  }
