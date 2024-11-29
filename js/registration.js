86764c9fe74a516871fc8dc941eac99e088485a6
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2024-11-29 23:04:54 +0530

    reg-page fixed

--- js/registration.js
+++ js/registration.js
@@ -15,3 +15,28 @@ addname();
     name.value="";
     addname();
   }
+
+const images=["./../images/registrationpage/img-1.jpg","./../images/registrationpage/img-2.jpg","./../images/registrationpage/img-3.jpg","./../images/registrationpage/img-4.jpg"]
+let index= 0;
+const slide= document.getElementById("slide");
+
+function next(){
+  if(index<images.length-1){
+    index++;
+  }
+  else{
+    index=0;
+  }
+  slide.src=images[index];
+}
+
+function prev(){
+  if(index>0){
+    index--;
+  }
+  else{
+    index=images.length-1;
+  }
+  slide.src=images[index];
+}
+setInterval(next,2000);
