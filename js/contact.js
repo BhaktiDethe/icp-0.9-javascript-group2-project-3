8725ec9c0bec14e0c22f2566e784a898cc17b13d
Vrush2004 <vrushalichaube@gmail.com>
2024-12-02 10:23:11 +0530

    made some changes

--- js/contact.js
+++ js/contact.js
@@ -7,17 +7,3 @@ function closeModel(){
     const overlayElement = document.getElementById('overlay');
     overlayElement.style.display= 'none';
 }
-const greetinText = document.getElementById('greetinText');
-const userName = localStorage.getItem('userName');
-const userInput = document.getElementById('userName');
-
-if(userName){
-    greetinText.innerText = `Hello ${userName}.....🖐`;
-}
-
-function greetUser(){
-    const userName = userInput.value;
-    localStorage.setItem('userName',userName);
-    greetinText.innerText = `Hello ${userName}.....🖐`;
-    userInput.value ='';
-}
