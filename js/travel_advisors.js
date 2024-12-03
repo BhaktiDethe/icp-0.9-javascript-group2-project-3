82bdea056c9993a0f1f1a04bc8ff1b3e95af6513
Vrush2004 <vrushalichaube@gmail.com>
2024-12-03 23:16:35 +0530

    changes done

--- js/travel_advisors.js
+++ js/travel_advisors.js
@@ -104,3 +104,28 @@
                         document.body.classList.remove('dark-mode')
                     }
                 })
+
+                const showMenu = (toggleId, navId) => {
+                    const toggle = document.getElementById(toggleId),
+                        nav = document.getElementById(navId),
+                        navbarEle = document.querySelector('.header');
+                 
+                    if (toggle && nav && navbarEle) {
+                        toggle.addEventListener('click', () => {
+                            const isMenuVisible = nav.classList.toggle('show-menu');
+                            toggle.classList.toggle('show-icon');
+                 
+                            if (isMenuVisible) {
+                                navbarEle.style.borderBottomLeftRadius = "0px";
+                                navbarEle.style.borderBottomRightRadius = "0px";
+                            } else {
+                                navbarEle.style.borderBottomLeftRadius = "20px";
+                                navbarEle.style.borderBottomRightRadius = "20px";
+                            }
+                        });
+                    } else {
+                        console.error("Toggle button, navigation menu, or navbar element not found!");
+                    }
+                 };
+                 
+                 showMenu('nav_toggle', 'nav_menu');
